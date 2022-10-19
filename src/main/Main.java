5c7d7b99cdab3f359ccea7a24795e1e77d0f9eac
PhyuPhway <phyuphyuphway1999@gmail.com>
2022-10-19 14:54:49 +0630

    Changed Main.java - add print stream

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git Version control");
+		System.out.println("Hello Git Version control-Changed1");
+
 	}
 }
